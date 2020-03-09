b27e2f8a97cc5dca398e2c70478b10f0db81107c
Nada <nadalina.t5@gmail.com>
2020-03-09 14:45:08 +0100

    dodata Scanner metoda i AppConsole

---
+++ Cas24/src/com/skolarajak/AppConsole.java
@@ -0,0 +1,103 @@
+package com.skolarajak;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+import com.skolarajak.model.Vozilo;
+import com.skolarajak.servisi.AdministriranjeVozila;
+
+public class AppConsole {
+	static final AdministriranjeVozila administracijaVozila = new AdministriranjeVozila(); // pretvoreno u staticku
+																							// metodu da bude vidljiva u
+																							// privatnim metodama za
+																							// euro2 i aktivna vozila i
+																							// na nivou je cele
+																							// aplikacije kada je
+																							// staticna
+
+	public static void main(String[] args) {
+		Date datum = new Date();
+		System.out.println("Pocetak rada aplikacije: " + datum.toString());
+
+		// generisi vozila
+
+		List<Vozilo> vozila = administracijaVozila.generisi(); // kreirali smo model sa dummy podacima
+
+		System.out.println("----------------Glavna programska petlja-------------------");
+
+		Scanner in = new Scanner(System.in);
+		while (1 == 1) { // obicno je glavna petlja korisnickog interfejsa bezkonacna, petlja drzi
+							// aplikaciju zivom, neki tread mora da drzi app zivom
+			prikaziOpcije();
+			System.out.println(">:");
+			String s = in.nextLine();
+			System.out.println("You entered a string " + s);
+			switch (s) {
+
+			case "0":
+				opcija0(vozila);
+				break;
+			case "1":
+				opcija1(vozila);
+				break;
+			case "2":
+				opcija2(vozila);
+				break;
+			}
+			if ("kraj".equalsIgnoreCase(s)) {
+				System.out.println("Kraj rada, hvala!");
+				break; // iz while petlje
+			}
+		}
+	}
+
+	private static void opcija0(List<Vozilo> vozila) {
+		System.out.println("========IZLISTAJ VOZILA=========");
+		System.out.println("Ukupno vozila: " + vozila.size());
+		izlistajVozila(vozila);
+	}
+
+	private static void opcija1(List<Vozilo> vozila) {
+		// izdvoj euro 3 vozila
+		System.out.println("========IZLISTAJ EURO 3 VOZILA=========");
+		List<Vozilo> euro3Vozila = administracijaVozila.euro3Vozila(vozila); // izdvoji euro3 vozila
+		System.out.println(euro3Vozila.size());
+		izlistajVozila(euro3Vozila);
+		System.out.println("--------------------------------------------------");
+	}
+
+	private static void opcija2(List<Vozilo> vozila) {
+		// izdvoj aktivna vozila
+		System.out.println("========IZLISTAJ AKTIVNA VOZILA=========");
+		List<Vozilo> aktivnaVozila = administracijaVozila.aktivnaVozila(vozila); // izdvoji euro3 vozila
+		System.out.println(aktivnaVozila.size());
+		izlistajVozila(aktivnaVozila);
+	}
+
+	private static void prikaziOpcije() { // rad sa app iz konzole
+		System.out.println("--------------------------------------------------");
+		System.out.println("0 -> Izlistaj vozila");
+		System.out.println("1 -> Izlistaj euro 3 vozila");
+		System.out.println("2 -> Izlistaj aktivna vozila");
+		System.out.println("kraj -> izlaz iz aplikacije");
+		System.out.println("--------------------------------------------------");
+	}
+
+	private static void izlistajVozila(List<Vozilo> vozila) { // pripada static void main metodi zato i ona mora biti
+																// staticka
+		/*
+		 * for (Vozilo vozilo : vozila) { printVozilo(vozilo); }
+		 */
+
+		vozila.forEach(AppConsole::printVozilo); // kroz listu stampamo za svaki element klase App, lambda izraz ali
+		// ima samo jedan ulazni parametar u metodi printVozilo
+		// metoda mora da ima samo jedan parametar da bi ovaj izraz radio za svaki
+		// elemenat
+	}
+
+	private static void printVozilo(Vozilo vozilo) {
+		System.out.println("Godiste: " + vozilo.getGodisteProizvodnje() + " Aktivno: " + vozilo.isAktivno()
+				+ " Registarski broj: " + vozilo.getRegistarskiBroj());
+	}
+}
--- Cas24/src/com/skolarajak/servisi/AdministriranjeVozila.java
+++ Cas24/src/com/skolarajak/servisi/AdministriranjeVozila.java
@@ -1,6 +1,7 @@
 package com.skolarajak.servisi;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,9 +11,11 @@ import com.skolarajak.utils.Konstante;
 public class AdministriranjeVozila {
 	private static final boolean STATUS = true;
 	private static final double PRAG_RASPODELE_AKTIVNIH_VOZILA = (double) 0.4; // kontrolisemo da li hocemo da imamo
-	private static final int SLOVO_A= 65;                                       // vise aktivnih i neaktivnih
-	private static final int SLOVO_Z= 90;
+	private static final int SLOVO_A = 65; // vise aktivnih i neaktivnih
+	private static final int SLOVO_Z = 90;
 	
+	private static final HashMap<String,String> registarskiBrojevi = new HashMap<String, String>();
+
 	public List<Vozilo> generisi() {
 		List<Vozilo> vozila = new ArrayList<Vozilo>();
 		for (int i = 0; i < Konstante.UKUPAN_BROJ_VOZILA_U_SISTEMU; i++) {
@@ -22,22 +25,22 @@ public class AdministriranjeVozila {
 			vozilo.setAktivno(Math.random() > PRAG_RASPODELE_AKTIVNIH_VOZILA); // logicki izraz, ako je veca od 0.5 bice
 																				// true, ako je manje jednako od 0.5
 																				// false
-			vozilo.setRegistarskiBroj("Reg-" + i + slucajnoSlovo() + slucajnoSlovo());
+			vozilo.setRegistarskiBroj(kreirajRegistarskiBroj());
 			vozila.add(vozilo);
 		}
+		System.out.println("Ukupno registarskih brojeva: " + registarskiBrojevi.keySet().size()); // koliko ima registarskih brojeva u HashMapi
 		return vozila;
 	}
 
 	public List<Vozilo> euro3Vozila(List<Vozilo> vozila) { // pocetna lista sadrzi sva vozila
 		/*
 		 * List<Vozilo> euro3Vozila = new ArrayList<Vozilo>(); // filtrirana privremena
-		 *                                                        lista rezultata samo euro 3 vozila
-		 * for(Vozilo vozilo : vozila) { // proveri elemente u listi
+		 * 															lista rezultata samo euro 3 vozila
+		 * for(Vozilo vozilo : vozila) { // proveri elemente u listi											
 		 *  if(vozilo.getGodisteProizvodnje() >= Konstante.EURO_3_GODISTE) { // ako odgovara uslovu 
-		 *  euro3Vozila.add(vozilo);                                         dodaje se u privremenu listu euro 3 vozila 
-		 * //                                                               
-		 * }
-		 * }
+		 *  euro3Vozila.add(vozilo);  //dodaje se u privremenu listu euro 3 vozila 
+		 *  } 
+		 *  }
 		 */
 
 		// Java 8 lambda izraz
@@ -48,13 +51,54 @@ public class AdministriranjeVozila {
 				.collect(Collectors.toList()); // rezultat vrati u listu kolektor u list
 		return euro3Vozila; // vrati euro 3 vozilo godiste >= 2000
 	}
-	
-	//?????
-	
-	public List<Vozilo> aktivnaVozila(List<Vozilo> vozila){
-		return null;
-	}
 
+	// ?????
+
+	public List<Vozilo> aktivnaVozila(List<Vozilo> vozila) {
+//		List<Vozilo> aktivnaVozila = new ArrayList<Vozilo>();
+//		for (Vozilo vozilo : vozila) {
+//			if (vozilo.isAktivno()) {
+//				aktivnaVozila.add(vozilo);
+//			}
+//		}
+//		return aktivnaVozila;
+//	}
+	
+		List<Vozilo> aktivnaVozila = vozila.stream()
+				.filter(v -> v.isAktivno())
+				.collect(Collectors.toList());
+			return aktivnaVozila;
+	}
+	
+	// kreiranje jedinstvenog registarskog broja sa HashMapom 
+	private String kreirajRegistarskiBroj() {
+//		String registarskiBroj ="Reg-" + slucajnoSlovo() + slucajnoSlovo();
+//		if (registarskiBrojevi.containsKey(registarskiBroj)) { // provera uslova da li registarski broj sadrzi key
+//			System.out.println("*************DUPLICAT**************" + registarskiBroj); // ispisemo regBr ako nadjemo iste registarske brojeve
+//			return kreirajRegistarskiBroj(); // onda ponovo pozivamo metodu u rekurziji
+//		}
+//		registarskiBrojevi.put(registarskiBroj, registarskiBroj); // ako ne sadrzi kljuc onda je nov kljuc i stavljamo ga u mapu, metoda kada je uspesno uradjena regiBroj je ubacen u mapu
+//		return registarskiBroj;
+	
+	//	boolean isJedinstvenRegBroj = false;  // nije jedinstven broj
+		
+		
+		String registarskiBroj = "";  // deklaracija iza petlje da bi se uradio return statment
+		
+	//	while(!isJedinstvenRegBroj) {  // ako je jedinstven
+		while(1==1){ // 2 varijanta beskrajna petlja iz koje se izlazi ako nadjemo jedinstven broj
+			registarskiBroj ="Reg-" + slucajnoSlovo() + slucajnoSlovo(); // kreiramo registarski broj
+			if (!registarskiBrojevi.containsKey(registarskiBroj)) { // proverimo da li se ne nalazi u mapi
+				registarskiBrojevi.put(registarskiBroj, registarskiBroj); // ako nije u mapi dodajemo ga u mapu
+				//isJedinstvenRegBroj = true; // umesto break-a 
+				break;
+			} else {
+				System.out.println("*************DUPLICAT**************" + registarskiBroj);
+			}
+		}
+		return registarskiBroj; 
+	}
+	
 	private int dodeliGodinuProizvodnje() {
 //		int godina =(int) (Math.random()*(Konstante.MAX_VOZILO_GODISTE - Konstante.MIN_VOZILO_GODISTE + 1) 
 //				+ Konstante.MIN_VOZILO_GODISTE); //vraca slucajnu godinu izmadju 1960-2000
